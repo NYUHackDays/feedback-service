bdadb8a825416bf1f436e735eba81fa47efbb0d3
Mariano Salinas <ms8310@nyu.edu>
2015-11-05 15:25:02 -0500

    Added all fields from each person to the array of members

--- feedback.py
+++ feedback.py
@@ -1,7 +1,7 @@
 import requests, json, secrets, smtplib
 
-headers = {'content-type': 'application/vnd.api+json', 'accept': 'application/*, text/*', 'authorization': 'Bearer ' + secrets.tnyu_api_key }
 
+headers = {'content-type': 'application/vnd.api+json', 'accept': 'application/*, text/*', 'authorization': 'Bearer ' + secrets.tnyu_api_key }
 
 
 def getEmails(eventId, eboard_members, attendees):
@@ -11,14 +11,11 @@ def getEmails(eventId, eboard_members, attendees):
 
 	
 	for post in r['included']:
-		info = [post['id']]
+		info = post
 		if post['attributes'].get('contact'):
 			if post['attributes']['roles']:
-				info.append(post['attributes']['contact']['email'])
-				eboard_members.append(info)
-				
+				eboard_members.append(info)			
 			else:
-				info.append(post['attributes']['contact']['email'])
 				attendees.append(info)
 	
 
@@ -27,9 +24,19 @@ def sendEmails(eboard_members, attendees):
 	server.starttls()
 	server.login(secrets.tnyu_email, secrets.tnyu_email_password)
 
+
+
 	for i in range (0, len(eboard_members)):
-		msg = 'Visit our website: https://julieycpan.typeform.com/to/AN1E2o?personId=' + eboard_members[i][0]
-		server.sendmail(secrets.tnyu_email, eboard_members[i][1], msg)
+
+		msg = "\r\n".join([
+			"From: " + secrets.tnyu_email,
+			"To: " + eboard_members[i]['attributes']['contact']['email'],
+			"Subject: Feedback for event",
+			"",
+			'Hi, ' + eboard_members[i]['attributes']['name'] + '\n\n' +
+			'Visit our website: https://julieycpan.typeform.com/to/AN1E2o?personId=' + eboard_members[i]['id']
+			])
+		server.sendmail(secrets.tnyu_email, eboard_members[i]['attributes']['contact']['email'], msg)
 
 	for j in range (0, len(attendees)):
 		msg = 'Visit our website: https://julieycpan.typeform.com/to/AN1E2o?personId=' + eboard_members[i][0]
@@ -46,11 +53,12 @@ def main():
 	eboard_members = []
 	attendees = []
 	getEmails('561491ec9d262920f265190c', eboard_members, attendees)
+	sendEmails(eboard_members,attendees)
 
 	print eboard_members
 	print attendees
 
-	sendEmails(eboard_members, attendees)
+	#sendEmails(eboard_members, attendees)
 
 main()
     
